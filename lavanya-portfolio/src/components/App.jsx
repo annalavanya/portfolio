552774128284c7cfa5cb18bbee53c7cc60223995
Anna Lavanya <annalavanya2002@gmail.com>
2025-06-27 22:51:08 +0530

    Basic project setup with vite and tailwindcss

---
+++ lavanya-portfolio/src/components/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-violet-300 text-pink-800 text-center'>Hi I am Lavanya.</h1>
+    </>
+  )
+}
+
+export default App
