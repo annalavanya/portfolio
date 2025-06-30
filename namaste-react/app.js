fd72956a9b17c49ec3d99dbfd5ee0e1084a2d24b
Anna Lavanya <annalavanya2002@gmail.com>
2025-06-30 17:47:35 +0530

    inception

---
+++ namaste-react/app.js
@@ -0,0 +1,36 @@
+/* <div id="parent"=>
+*   <div id="child1">
+*       <h1>This is a h1 tag
+*       </h1>
+*       <h2> This is h2
+*       </h2>
+*   </div> 
+*   <div id="child2">
+*       <h1>This is a h1 tag
+*       </h1>
+*       <h2> This is h2
+*       </h2>
+*   </div> 
+* </div> 
+*/ 
+
+const heading = React.createElement("div", 
+    {
+        id:"parent"
+    }, 
+    [
+        React.createElement("div", { id: "child1"}, [
+            React.createElement("h1", {}, "This is a h1 tag"),
+            React.createElement("h2", {}, "This is a h2 tag")
+        ]),
+        React.createElement("div", { id: "child2"}, [
+            React.createElement("h1", {}, "This is a h1 tag"),
+            React.createElement("h2", {}, "This is a h2 tag")
+        ]),
+    ]
+);
+
+console.log("heading", heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
