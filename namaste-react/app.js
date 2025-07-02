fe025703bf3d73d73baab493994817e276abe58b
Anna Lavanya <annalavanya2002@gmail.com>
2025-07-02 17:14:28 +0530

    second

--- namaste-react/app.js
+++ namaste-react/app.js
@@ -14,17 +14,19 @@
 * </div> 
 */ 
 
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("div", 
     {
         id:"parent"
     }, 
     [
         React.createElement("div", { id: "child1"}, [
-            React.createElement("h1", {}, "This is a h1 tag"),
-            React.createElement("h2", {}, "This is a h2 tag")
+            React.createElement("h1", {}, "This is a h1"),
+            React.createElement("h2", {}, "This is a h2")
         ]),
         React.createElement("div", { id: "child2"}, [
-            React.createElement("h1", {}, "This is a h1 tag"),
+            React.createElement("h1", {}, "This is a h1"),
             React.createElement("h2", {}, "This is a h2 tag")
         ]),
     ]
