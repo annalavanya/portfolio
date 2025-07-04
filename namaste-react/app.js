8be2976d3be8703590564ca6fcb688f5b8fd6eb3
Anna Lavanya <annalavanya2002@gmail.com>
2025-07-04 16:49:57 +0530

    laying the foundation

--- namaste-react/app.js
+++ namaste-react/app.js
@@ -16,23 +16,26 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "react/jsx-dev-runtime";
 const heading = React.createElement("div", 
     {
         id:"parent"
-    }, 
-    [
-        React.createElement("div", { id: "child1"}, [
-            React.createElement("h1", {}, "This is a h1"),
-            React.createElement("h2", {}, "This is a h2")
-        ]),
-        React.createElement("div", { id: "child2"}, [
-            React.createElement("h1", {}, "This is a h1"),
-            React.createElement("h2", {}, "This is a h2 tag")
-        ]),
-    ]
+    },
+    "This is lavanya from React"
 );
 
 console.log("heading", heading);
 
+// react element
+const jsxHeading = <h1 id="heading">This is Variable 1</h1>
+console.log(jsxHeading);
+
+const jsxHeading1 = <div>{jsxHeading} <h1>This is Variable 2</h1></div>
+
+//react component
+const FirstComponent = () => { return <div>{jsxHeading1}<h1 id="heading">First Component </h1></div>};
+
+const SecondComponent = () => <div>{FirstComponent()}<FirstComponent/><FirstComponent></FirstComponent><h1 id="heading">Second Component </h1></div>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<SecondComponent/>);
